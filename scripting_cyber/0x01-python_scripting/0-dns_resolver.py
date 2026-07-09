32b20dc0f58e077db2a32c8b7698a54ec2fe80f6
LuisFilipePires <learn2b3@gmail.com>
2026-07-09 16:16:04 -0500

    IPv4 DNS Resolution

---
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""DNS resolver module"""
+
+import socket
+
+
+def resolve_domain_to_ipv4(domain_name):
+    """Resolve a domain name to its IPv4 address."""
+
+    try:
+        return socket.gethostbyname(domain_name)
+    except socket.gaierror:
+        return None
+    except Exception as e:
+        return str(e)
