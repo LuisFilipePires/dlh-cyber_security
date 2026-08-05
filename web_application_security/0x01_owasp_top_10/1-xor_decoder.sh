c403998330dd64f169847c781bba32fa529900e1
LuisfilipePires <learn2b3@gmail.com>
2026-08-05 11:16:29 +0200

    add xor decoder

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
 HASH="${1#\{xor\}}"
-echo "$HASH" | base64 -d | python3 -c '
+
+python3 -c '
+import base64
+
+data = "'"$HASH"'"
+
+decoded = base64.b64decode(data)
+
+key = [0x5f]
+
+result = bytes(
+    decoded[i] ^ key[i % len(key)]
+    for i in range(len(decoded))
+)
+
+print(result.decode())
+'
